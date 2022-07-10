42f3b58af5d56d1b089d13ad6ded639a50708fc0
jreyes1990 <jarl1990mt@gmail.com>
2022-07-09 23:11:42 -0600

    Ejercicios de python

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,10 @@
 '''Escribir un programa que realice la siguiente operación aritmética: (3+2)/(2*5) elevado a la 2'''
 
+calculo1 = pow(((3+2)/(2*5)),2)
+calculo2 = ((3+2)/(2*5))**2
+
 print(pow(((3+2)/(2*5)),2))
+print(calculo1)
 
 print(((3+2)/(2*5))**2)
+print(calculo2)
