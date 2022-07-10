52c58c3b22b206a6a1000449ab484571b5de1ae3
jreyes1990 <jarl1990mt@gmail.com>
2022-07-10 11:04:53 -0600

    Indices y Slicing

---
+++ indiceAsociados.py
@@ -0,0 +1,7 @@
+'''Indices y Slicing: Los indices son numeros ordenados a las variables que nos permite identificar
+   los elementos por su posicion'''
+
+cadena = "Cadena texto"
+
+print(cadena[0],cadena[1],cadena[2],cadena[3],cadena[4],cadena[5])
+print(cadena[-5],cadena[-4],cadena[-3],cadena[-2],cadena[-1])
