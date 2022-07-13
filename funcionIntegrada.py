193f846844fe4a9f06929f558e903e8187f71a09
jreyes1990 <jarl1990mt@gmail.com>
2022-07-12 19:00:09 -0600

    Funciones Integradas

---
+++ funcionIntegrada.py
@@ -0,0 +1,19 @@
+i = 10
+d = 10.5
+
+print(f"Tipo de dato -> {type(i)}")
+print(f"Tipo de dato -> {type(d)}")
+print(f"Binario de 10-> {bin(10)}")
+print(f"Hexadecimal de 10-> {hex(10)}")
+print(f"Valor absoluto de -10-> {abs(-10)}")
+print(f"Redondeo de 10.7-> {round(10.7)}")
+print(f"Potencia de 10-> {pow(10,2)}")
+
+
+texto = "Para separar una cadena"
+print(f"Separando cadena-> {texto.split()}")
+print(f"Mayuscula-> {texto.upper()}")
+print(f"Minuscula-> {texto.lower()}")
+print(f"Capitalize-> {texto.title()}")
+
+print(f"Diccionario-> {dict(zip(['A','B','C','D'],[10,12,13,14]))}")
